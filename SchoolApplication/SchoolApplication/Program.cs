a218cd12ed478d4a3a76b2c349b70dc5854ec9a8
velibeyli <valibayli91@gmail.com>
2021-12-30 01:12:44 +0400

    Student Application created

--- SchoolApplication/SchoolApplication/Program.cs
+++ SchoolApplication/SchoolApplication/Program.cs
@@ -10,6 +10,26 @@ namespace SchoolApplication
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Student student = new Student();
+
+            Console.WriteLine("Adınızı daxil edin: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Soyadınızı daxil edin: ");
+            student.Surname = Console.ReadLine();
+
+            Console.WriteLine("Doğum tarixini daxil edin: ");
+            student.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Adınız: {0}",student.Name);
+            Console.WriteLine("Soyadınız: {0}", student.Surname);
+            Console.WriteLine("Doğum tarixiniz: {0}", student.DateOfBirth.ToString("dd MMMM yyyy"));
+
+            Console.ReadLine();
         }
     }
 }
